8212122c12ebe38b3538fddd2ad1e45839f7515e
mahedee <mahedee.hasan@gmail.com>
2023-06-01 22:29:50 -0300

    Add CartItemWithBuyOneGetOneFree_Quantity1_ShouldReturnWithoutPromotionPrice

---
+++ backend/tests/Klir.TechChallenge.Tests/PriceCalculatorTest.cs
@@ -0,0 +1,15 @@
+namespace Klir.TechChallenge.Tests;
+
+public class PriceCalculatorTest
+{
+    public PriceCalculatorTest()
+    {
+        
+    }
+
+    [Fact]
+    public void CartItemWithBuyOneGetOneFree_Quantity1_ShouldReturnWithoutPromotionPrice()
+    {
+
+    }
+}
